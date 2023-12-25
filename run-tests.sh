79ab14ec298371af98c33cadba4727252e6a8886
benjamin.combe <benjamincombe@outlook.fr>
2023-12-25 09:35:08 +0100

    add php 8.3 support and Symfony 7

--- run-tests.sh
+++ run-tests.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -it --rm -u ${UID} -v `pwd`:/app -v `pwd`/.composer:/.composer -w /app kilik/php:8.0-bullseye-dev vendor/bin/simple-phpunit
+docker run -it --rm -u ${UID} -v `pwd`:/app -v `pwd`/.composer:/.composer -w /app kilik/php:8.3-dev vendor/bin/simple-phpunit
